449ea9d3dd9aab61328eabb23cd02ee975e0c1f3
Pham Hoang Son <sonpham3798@gmail.com>
2024-03-27 11:29:41 +0700

    remove unused

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 // NextJS Middleware
 // This function can be marked `async` if using `await` inside
 // ref: https://nextjs.org/docs/app/building-your-application/routing/middleware
-export async function middleware(request: NextRequest) {
+export async function middleware() {
   return NextResponse.next();
 }
 
